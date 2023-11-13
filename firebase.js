1ad73f1797a9fe1c75698875676684b04ab0d971
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2023-11-13 05:44:34 -0600

    Comment out Firebase,js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,3 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export { app, analytics };
+*/
