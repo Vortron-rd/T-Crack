a5e6e4268367f488ff341b218b9bdddf6f6f538c
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2024-11-22 05:07:21 -0600

    Changed index.html to immediately open a cloaked version of its old form, stored in a new file, main.html. Replaced "Cloak" button with "Open in new tab button" which will simply open main.html in a new cloaked tab. Cleaned Games list.

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
-// JavaScript code to load and play web games, and toggle fullscreen mode
+//Opens a about:blank tab with either a <iframe> or a <embed>
+function aboutBlank(url, type) {
+  var urlObj = new window.URL(window.location.href);
+  if(url) {
+    var win;
+    if(win) {
+      win.focus();
+    } else {
+      win = window.open();
+      win.document.body.style.margin = '0';
+      win.document.body.style.height = '100vh';
+      var iframe = win.document.createElement(type);
+      iframe.style.border = 'none';
+      iframe.style.width = '100%';
+      iframe.style.height = '100%';
+      iframe.style.margin = '0';
+      iframe.src = url;
+      win.document.body.appendChild(iframe);
+      win.document.title = "IXL | Math, Language Arts, Science, Social Studies, and Spanish"
+      var link = document.querySelector("link[rel~='icon']");
+      if(!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+      }
+      link.href = 'https://stackoverflow.com/favicon.ico';
+    }
+  };
+}
 
-let isFullscreen = false; // Variable to keep track of fullscreen state
+//Replaces the current tab with the specified url and (usually) overwrites history https://
+
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+function replace(url) {
+	window.location.replace(url);
+}
+
 
 // Function to load a game into the game container
 function loadGame(gameUrl) {
   const gameContainer = document.getElementById('game-container');
   gameContainer.innerHTML = `<embed src="${gameUrl}" width="100%" height="100%" frameborder="0"></embed>`;
 }
+//Function ran when index.html is loaded, loads about:blank main.html and replaces parent with ixl.com
+function init() {
+str = window.location.href;
+mainpage = (str.substring(0, str.lastIndexOf("/"))) + '/main.html';
+aboutBlank(mainpage, "embed");
+replace('https://ixl.com');
+}
+//Function to input custom URL into a load game function
 function loadCustomGame() {
-  loadGame(document.getElementById('url-input').value)
+  loadGame(document.getElementById('url-input').value);
 }
 // Function to toggle fullscreen mode
 function toggleFullscreen() {
   const gameContainer = document.getElementById('game-container');
-  const fullscreenBtn = document.getElementById('fullscreen-btn');
 
-  if (!isFullscreen) {
+
     if (gameContainer.requestFullscreen) {
       gameContainer.requestFullscreen();
     } else if (gameContainer.mozRequestFullScreen) {
@@ -24,6 +78,5 @@ function toggleFullscreen() {
       gameContainer.webkitRequestFullscreen();
     } else if (gameContainer.msRequestFullscreen) {
       gameContainer.msRequestFullscreen();
-    }
-  } 
+    } 
 }
